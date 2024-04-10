367611bb8ef191a0586c8d6bfc4d948fc9d5bac3
Owen <osmour043@gmail.com>
2024-04-09 22:47:14 -0500

    linting

--- src/ScraperFC/Capology.py
+++ src/ScraperFC/Capology.py
@@ -5,53 +5,41 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import StaleElementReferenceException
 import pandas as pd
-from IPython.display import clear_output
-
 from ScraperFC.shared_functions import get_source_comp_info, InvalidCurrencyException
+
 
 class Capology():
 
-    ############################################################################
+    # ==========================================================================
     def __init__(self):
         options = Options()
         options.add_argument('--headless')
-        prefs = {'profile.managed_default_content_settings.images': 2} # don't load images
+        prefs = {'profile.managed_default_content_settings.images': 2}  # don't load images
         options.add_experimental_option('prefs', prefs)
         self.driver = webdriver.Chrome(options=options)
 
-        self.leagues = {
-            'Bundesliga': 'de/1-bundesliga',
-            '2.Bundesliga': '/de/2-bundesliga',
-            'EPL': 'uk/premier-league',
-            'EFL Championship': '/uk/championship',
-            'Serie A': 'it/serie-a',
-            'Serie B': 'it/serie-b',
-            'La Liga': 'es/la-liga',
-            'La Liga 2': 'es/la-liga-2',
-            'Ligue 1': 'fr/ligue-1',
-            'Ligue 2': 'fr/ligue-2',
-            'Eredivisie': '/ne/eredivisie',
-            'Primeira Liga': '/pt/primeira-liga',
-            'Scottish PL': '/uk/scottish-premiership',
-            'Super Lig': '/tr/super-lig',
-            'Belgian 1st Division': 'be/first-division-a'
-        }
+        self.leagues = {'Bundesliga': 'de/1-bundesliga', '2.Bundesliga': '/de/2-bundesliga',
+                        'EPL': 'uk/premier-league', 'EFL Championship': '/uk/championship',
+                        'Serie A': 'it/serie-a', 'Serie B': 'it/serie-b',
+                        'La Liga': 'es/la-liga', 'La Liga 2': 'es/la-liga-2',
+                        'Ligue 1': 'fr/ligue-1', 'Ligue 2': 'fr/ligue-2',
+                        'Eredivisie': '/ne/eredivisie', 'Primeira Liga': '/pt/primeira-liga',
+                        'Scottish PL': '/uk/scottish-premiership', 'Super Lig': '/tr/super-lig',
+                        'Belgian 1st Division': 'be/first-division-a'}
 
         self.valid_currencies = ['eur', 'gbp', 'usd']
 
-
-    ############################################################################
+    # ==========================================================================
     def close(self):
         """ Closes and quits the Selenium WebDriver instance.
         """
         self.driver.close()
         self.driver.quit()
 
-    
-    ############################################################################
+    # ==========================================================================
     def scrape_salaries(self, year, league, currency):
         """ Scrapes player salaries for the given league season.
-        
+
         Args
         ----
         year : int
@@ -71,60 +59,45 @@ class Capology():
         if currency not in self.valid_currencies:
             raise InvalidCurrencyException()
 
-        league_url = f'https://www.capology.com/{self.leagues[league]}/salaries/{year-1}-{year}'
+        league_url = f'https://www.capology.com/{self.leagues[league]}/salaries/{year - 1}-{year}'
         self.driver.get(league_url)
 
         # Show all players on one page -----------------------------------------
         done = False
         while not done:
             try:
-                all_btn = WebDriverWait(
-                    self.driver, 
-                    10,
-                ).until(EC.element_to_be_clickable(
-                    (By.LINK_TEXT, 'All')
-                ))
+                all_btn = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, 'All')))
                 all_btn.click()
                 done = True
             except StaleElementReferenceException:
                 pass
 
         # Select the currency --------------------------------------------------
-        currency_btn = WebDriverWait(
-            self.driver, 
-            10,
-        ).until(EC.element_to_be_clickable(
-            (By.ID, 'btn_{}'.format(currency))
-        ))
+        currency_btn = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, f'btn_{currency}')))
         self.driver.execute_script('arguments[0].click()', currency_btn)
         print('Changed currency')
 
         # Table to pandas df ---------------------------------------------------
         tbody_html = self.driver.find_element(By.ID, 'table')\
-                .find_element(By.TAG_NAME, 'tbody')\
-                .get_attribute('outerHTML')
+                                .find_element(By.TAG_NAME, 'tbody')\
+                                .get_attribute('outerHTML')
         table_html = '<table>' + tbody_html + '</table>'
         df = pd.read_html(table_html)[0]
         if df.shape[1] == 13:
             df = df.drop(columns=[1])
-            df.columns = [
-                'Player', 'Weekly Gross', 'Annual Gross', 'Expiration', 'Length', 
-                'Total Gross', 'Status', 'Pos. group', 'Pos.', 'Age', 'Country', 
-                'Club'
-            ]
+            df.columns = ['Player', 'Weekly Gross', 'Annual Gross', 'Expiration',
+                          'Length', 'Total Gross', 'Status', 'Pos. group', 'Pos.',
+                          'Age', 'Country', 'Club']
         else:
-            df.columns = [
-                'Player', 'Weekly Gross', 'Annual Gross', 'Adj. Gross', 'Pos. group', 
-                'Age', 'Country', 'Club'
-            ]
+            df.columns = ['Player', 'Weekly Gross', 'Annual Gross', 'Adj. Gross',
+                          'Pos. group', 'Age', 'Country', 'Club']
 
         return df
 
-    
-    ############################################################################
+    # ==========================================================================
     def scrape_payrolls(self, year, league, currency):
         """ Scrapes team payrolls for the given league season.
-        
+
         Args
         ----
         year : int
@@ -144,31 +117,20 @@ class Capology():
         if currency not in self.valid_currencies:
             raise InvalidCurrencyException()
 
-        
-        league_url = 'https://www.capology.com/{}/payrolls/{}-{}'.format(self.leagues[league], year-1, year)
+        league_url = f'https://www.capology.com/{self.leagues[league]}/payrolls/{year - 1}-{year}'
 
         self.driver.get(league_url)
 
         # select the currency
-        currency_btn = WebDriverWait(
-            self.driver, 
-            10,
-        ).until(EC.element_to_be_clickable(
-            (By.ID, 'btn_{}'.format(currency))
-        ))
+        currency_btn = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, f'btn_{currency}')))
         self.driver.execute_script('arguments[0].click()', currency_btn)
 
         # table to pandas df
-        table = WebDriverWait(
-            self.driver, 
-            10,
-        ).until(EC.element_to_be_clickable(
-            (By.ID, 'table')
-        ))
+        table = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'table')))
         done = False
         while not done:
             df = pd.read_html(table.get_attribute('outerHTML'))[0]
-            done = True if df.shape[0]>0 else False
+            done = True if df.shape[0] > 0 else False
         # df.columns = [
         #     'Club', 'Weekly Gross (000s)', 'Annual Gross (000s)', 'Inflcation-Adj. Gross (000s)',
         #     'Keeper (000s)', 'Defense (000s)', 'Midfield (000s)', 'Forward (000s)'
--- src/ScraperFC/ClubElo.py
+++ src/ScraperFC/ClubElo.py
@@ -1,15 +1,14 @@
 from datetime import datetime
 from io import StringIO
-from IPython.display import clear_output
 import pandas as pd
 import requests
-import time
 
-class ClubElo:        
-        
+
+class ClubElo:
+
+    # ==========================================================================
     def scrape_team_on_date(self, team, date):
         """ Scrapes a team's ELO score on a given date.
-
         Args
         ----
         team : str
@@ -26,9 +25,9 @@ class ClubElo:
             -1 if the team has no score on the given date
         """
         date = datetime.strptime(date, '%Y-%m-%d')
-        
+
         # use ClubElo API to get team data as Pandas DataFrame
-        url = 'http://api.clubelo.com/{}'.format(team)
+        url = f'http://api.clubelo.com/{team}'
         done = False
         while not done:
             try:
@@ -37,13 +36,12 @@ class ClubElo:
             except requests.exceptions.ConnectionError:
                 done = False
         df = pd.read_csv(StringIO(r.text), sep=',')
-        
+
         # find row that given date falls in
         for i in df.index:
-            from_date = datetime.strptime(df.loc[i,'From'], '%Y-%m-%d')
-            to_date = datetime.strptime(df.loc[i,'To'], '%Y-%m-%d')
-            if (date>from_date and date<to_date) or date==from_date or date==to_date:
-                return df.loc[i,'Elo']
-        
-        return -1 # return -1 if ELO not found for given date
-        
+            from_date = datetime.strptime(df.loc[i, 'From'], '%Y-%m-%d')
+            to_date = datetime.strptime(df.loc[i, 'To'], '%Y-%m-%d')
+            if (date > from_date and date < to_date) or date == from_date or date == to_date:
+                return df.loc[i, 'Elo']
+
+        return -1  # return -1 if ELO not found for given date
