b663c795aa8b2913f5ece3fa2e74019d8df1dfd6
Owen <osmour043@gmail.com>
2024-04-26 10:39:33 -0500

    FiveThirtyEight unit tests

--- src/ScraperFC/FiveThirtyEight.py
+++ src/ScraperFC/FiveThirtyEight.py
@@ -1,3 +1,6 @@
+from .shared_functions import xpath_soup
+from .scraperfc_exceptions import InvalidLeagueException, InvalidYearException
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -6,113 +9,97 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 import pandas as pd
 from zipfile import ZipFile
-from ScraperFC.shared_functions import get_source_comp_info, xpath_soup
 import time
 from bs4 import BeautifulSoup
 
 
 class FiveThirtyEight:
     
-    # ==========================================================================
+    # ==============================================================================================
     def __init__(self):
+        return
+
+    # ==============================================================================================
+    def _webdriver_init(self):
+        """ Creates a selenium webdriver instance
+        """
         options = Options()
         options.add_argument('--headless')
         prefs = {'download.default_directory': os.getcwd()}
         options.add_experimental_option('prefs', prefs)
         self.driver = webdriver.Chrome(options=options)
         
-    # ==========================================================================    
-    def close(self):
-        ''' Closes and quits the Selenium WebDriver instance.
-        '''
+    # ==============================================================================================    
+    def _webdriver_close(self):
+        """ Closes and quits the Selenium WebDriver instance.
+        """
         self.driver.close()
         self.driver.quit()
         
-    # ==========================================================================    
-    def up_season(self, string):
-        ''' Increments a string of the season year
-        
-        Args
-        ----
-        string : str
-            String of a calendar year (e.g. '2022')
-        Returns
-        -------
-        : str
-            Incremented calendar year
-        '''
-        return str(int(string) + 1)
-        
-    # ==========================================================================  
-    def scrape_matches(self, year, league, save=False):
-        ''' Scrapes matches for the given league season
+    # ==============================================================================================  
+    def scrape_matches(self, year, league):
+        """ Scrapes matches for the given league season
 
         Args
         ----
         year : int
-            Calendar year that the season ends in (e.g. 2023 for the 2022/23 season)
+            Calendar year that the season begins in (e.g. 2022 for the 2022/23 season). This is 
+            different from FiveThirtyEight versus other ScraperFC modules.
         league : str
-            League. Look in shared_functions.py for the available leagues for each\
-            module.
-        save : bool
-            OPTIONAL, default is False. If True, output will be saved to a CSV file.
+            League. Look in shared_functions.py for the available leagues for each module.
         Returns
         -------
         : Pandas DataFrame
-            If save=False, FiveThirtyEight stats for all matches of the given\
-            league season
-        filename : str
-            If save=True, filename of the CSV that the stats were saved to 
-        '''
-        _ = get_source_comp_info(year, league, 'FiveThirtyEight')
+            If save=False, FiveThirtyEight stats for all matches of the given league season
+        """
+        if type(year) is not int and year != 'All':
+            raise TypeError('`year` must be an int.')
+        if type(league) is not str:
+            raise TypeError('`league` must be a string.')
         
-        # Load URL
-        self.driver.get('https://data.fivethirtyeight.com/#soccer-spi')
-        
-        # Wait for data index to be available
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'dataIndex')))
+        self._webdriver_init()
 
-        # Click download button
-        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
-        button_xpath = xpath_soup(soup.find('div', {'dataset-name': 'soccer-spi'}))
-        button = self.driver.find_element(By.XPATH, button_xpath)
-        self.driver.execute_script('arguments[0].click();', button)
-                
-        # Wait for download to complete
-        while not os.path.exists('soccer-spi.zip'):
-            time.sleep(1)
+        try:
+            # Load URL
+            self.driver.get('https://data.fivethirtyeight.com/#soccer-spi')
+            
+            # Wait for data index to be available
+            WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'dataIndex')))
+
+            # Click download button
+            soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+            button_xpath = xpath_soup(soup.find('div', {'dataset-name': 'soccer-spi'}))
+            button = self.driver.find_element(By.XPATH, button_xpath)
+            self.driver.execute_script('arguments[0].click();', button)
+                    
+            # Wait for download to complete
+            while not os.path.exists('soccer-spi.zip'):
+                time.sleep(1)
+            
+            # Get data table
+            with ZipFile('soccer-spi.zip') as zf:
+                with zf.open('soccer-spi/spi_matches.csv') as f:
+                    df = pd.read_csv(f)
+                    
+            # Delete downloaded folder
+            os.remove('soccer-spi.zip')
         
-        # Get data table
-        with ZipFile('soccer-spi.zip') as zf:
-            with zf.open('soccer-spi/spi_matches.csv') as f:
-                df = pd.read_csv(f)
-                
-        # Delete downloaded folder
-        os.remove('soccer-spi.zip')
-        
-        # Pick the chosen league
-        if league == 'EPL':
-            df = df[df['league'] == 'Barclays Premier League']
-        elif league == 'La Liga':
-            df = df[df['league'] == 'Spanish Primera Division']
-        elif league == 'Bundesliga':
-            df = df[df['league'] == 'German Bundesliga']
-        elif league == 'Serie A':
-            df = df[df['league'] == 'Italy Serie A']
-        elif league == 'Ligue 1':
-            df = df[df['league'] == 'French Ligue 1']
-        
-        # Add one to season column
-        df['season'] = df['season'].apply(self.up_season)
-        
-        # Only keep the season requested
-        df = df[df['season'] == str(year)].reset_index(drop=True)
-        
-        # Save to CSV if requested by user
-        if save:
-            filename = f'{year}_{league}_FiveThirtyEight_matches.csv'
-            df.to_csv(path_or_buf=filename, index=False)
-            print('Matches dataframe saved to ' + filename)
-            return filename
-        else:
+            # Pick the chosen league or keep all
+            if league == 'All':
+                pass
+            elif league not in df['league'].values:
+                raise InvalidLeagueException(league, 'FiveThirtyEight')
+            else:
+                df = df[df['league'] == league]
+
+            # Get the requested year or keep all
+            if year == 'All':
+                pass
+            elif year not in df['season'].values:
+                raise InvalidYearException(year, league)
+            else:
+                df = df[df['season'] == year].reset_index(drop=True)
+
             return df
+        finally:
+            self._webdriver_close()
---
+++ test/test_FiveThirtyEight.py
@@ -0,0 +1,41 @@
+import sys
+sys.path.append('./src/')
+from ScraperFC import FiveThirtyEight
+from ScraperFC.scraperfc_exceptions import InvalidLeagueException, InvalidYearException
+
+import pytest
+import pandas as pd
+from contextlib import nullcontext as does_not_raise
+
+
+class TestFiveThirtyEight:
+
+    @pytest.mark.parametrize(
+        'year, league, expected',
+        [(2020, 'La Liga', pytest.raises(InvalidLeagueException)),
+         (2020, 9, pytest.raises(TypeError))]
+    )
+    def test_invalid_league(self, year, league, expected):
+        with expected:
+            FiveThirtyEight().scrape_matches(year, league)
+    
+    @pytest.mark.parametrize(
+        'year, league, expected',
+        [(2015, 'All', pytest.raises(InvalidYearException)),
+         ('a', 'Barclays Premier League', pytest.raises(TypeError)),
+         ('All', 'German Bundesliga', does_not_raise())]
+    )
+    def test_invalid_year(self, year, league, expected):
+        with expected:
+            FiveThirtyEight().scrape_matches(year, league)
+    
+    @pytest.mark.parametrize(
+        'year, league',
+        [('All', 'All'), (2020, 'Spanish Primera Division'), (2017, 'German Bundesliga'),
+         ('All', 'English League One'), (2023, 'All')]
+    )
+    def test_scrape_matches(self, year, league):
+        df = FiveThirtyEight().scrape_matches(year, league)
+        assert type(df) is pd.DataFrame
+        assert df.shape[0] > 0
+        assert df.shape[1] > 0
